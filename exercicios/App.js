a47faeaea59b4f22203a92cc2a6888406076fa53
Marcos Mariano <marcos.fernandes@aedb.br>
2020-07-10 10:34:38 -0300

    Versão 1.0 - Exemplo

---
+++ exercicios/App.js
@@ -0,0 +1,13 @@
+      import React from 'react'
+      import { Text, } from 'react-native'
+      import Estilo from './src/components/estilo'
+    
+      export default props => {
+      	return (
+        	<Text style={Estilo.txtG}>Exemplo
+
+            </Text>
+
+        	)
+    }
+ 
---
+++ exercicios/src/components/estilo.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    txtG: {
+        fontSize: 32,
+        textAlign: "center",
+        backgroundColor: "#007",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 100
+        
+    }
+})
